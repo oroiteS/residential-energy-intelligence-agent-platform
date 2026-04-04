a21e62cf220340587309c222abc85be8c44eaed3
oroiteS <oroite@qq.com>
2026-04-04 23:58:30 +0800

    restore transformer package files

---
+++ models/forecast/transformer/constants.py
@@ -0,0 +1,5 @@
+"""Decoder-only Transformer 预测常量。"""
+
+from forecast.LSTM.constants import ALL_FEATURE_NAMES, INPUT_LENGTH, TARGET_LENGTH
+
+__all__ = ["ALL_FEATURE_NAMES", "INPUT_LENGTH", "TARGET_LENGTH"]
